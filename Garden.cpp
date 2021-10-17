48e5293d9df29f8a2e73a177ba01589270fe720b
22Karthikeyan22 <86094734+22Karthikeyan22@users.noreply.github.com>
2021-10-17 21:03:00 +0530

    Update and rename Garden to Garden.cpp

--- Garden.cpp
+++ Garden.cpp
@@ -1,4 +1,4 @@
-Garden
+/*Garden
 A group of childrens were playing in the garden and they need to form a circle based on the radius and the respected area so help them to find the radius of the area to form a circle by creating a class in c++.
 Input Format:
 
@@ -26,3 +26,28 @@ Input (stdin)
 Output (stdout)
 Enter the value of Radius :
 Area of Circle : 113.04
+solu:-*/
+#include<iostream>
+using namespace std;
+class Circle
+{
+  private:
+  	int radius;
+  public:
+  	void readRadius()
+    {
+      cout << "Enter the value of Radius :" << endl;
+      cin >> radius;
+    }
+  	void printArea()
+    {
+      cout << "Area of Circle : " << 3.14*radius*radius;
+    }
+};
+int main()
+{
+  //Type your code here...
+  Circle c;
+  c.readRadius();
+  c.printArea();
+}
