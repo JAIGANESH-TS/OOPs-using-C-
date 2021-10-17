67829be62492ff5b99f2ba7f7b17631f238c4f66
22Karthikeyan22 <86094734+22Karthikeyan22@users.noreply.github.com>
2021-10-17 20:54:20 +0530

    Update and rename Time Machine to Time Machine.cpp

--- Time Machine.cpp
+++ Time Machine.cpp
@@ -1,4 +1,4 @@
-Time Machine
+/*Time Machine
 Jethro and Jairo were developing a time machine. Jehtro can do a machine in certain hours,minutes and seconds where as Jairo can do a same work in different hours,minutes and seconds. Help the Einstein to find the total time for developing the time machine by creating a class and objects in c++.
 Input Format:
 
@@ -62,3 +62,33 @@ Hours :
 Minutes :
 Seconds :
 Time after add: 12:11:57
+sol:-*/
+#include<iostream>
+using namespace std;
+class Time{
+  public:
+  	int h,m,s;
+  	Time(){
+      cout << "Enter time:" << endl;
+      cout << "Hours :" << endl;
+      cin >> h;
+      cout << "Minutes :" << endl;
+      cin >> m;
+      cout << "Seconds :" << endl;
+      cin >> s;
+    }
+  	void add(Time b){
+      int x=h+b.h,y=m+b.m,z=s+b.s;
+      y+=z/60;
+      z%=60;
+      x+=y/60;
+      y%=60;
+      cout << "Time after add: " << x << ":" << y << ":" << z;
+    }
+};
+int main()
+{
+  //Type your code here...
+  Time a,b;
+  a.add(b);
+}
