a4b86b4f6b05a6f68c58b863d1d758422d976609
22Karthikeyan22 <86094734+22Karthikeyan22@users.noreply.github.com>
2021-10-17 21:07:36 +0530

    Update and rename BANK to BANK.cpp

--- BANK.cpp
+++ BANK.cpp
@@ -1,4 +1,4 @@
-Bank
+/*Bank
 Niranjan goes to the bank for depositing money in his friend account. And he is having the certain details to deposit. After depositing the opponent withdrawn certain amount. But the opponent is unable to find the balance in his account. Help him to find the balance amount in his account by creating a class using c++.
 Input Format:
 
@@ -68,3 +68,54 @@ Accout No. : 5346
 Name : Jeshua
 Account Type : Savings
 Balance : 51000
+solu:-*/
+#include<iostream>
+using namespace std;
+class Bank{
+  private:
+  int acno, bal;
+  string name, type;
+  public:
+  void getDetails(){
+    cout << "Enter Details:" << endl;
+    cout << "Account No." << endl;
+    cin >> acno;
+    cout << "Name :" << endl;
+    cin >> name;
+    cout << "Account Type :" << endl;
+    cin >> type;
+    cout << "Balance :" << endl;
+    cin >> bal;
+  }
+  void deposit(){
+    int dep;
+    cout << "Enter Deposit Amount =" << endl;
+    cin >> dep;
+    bal+=dep;
+  }
+  void withdraw(){
+    int with;
+    cout << "Enter Withdraw Amount =" << endl;
+    cin >> with;
+     bal-=with;
+    if (with>bal){
+      cout << "Cannot Withdraw Amount" << endl;
+    }
+   
+  }
+  void printDetails(){
+    cout << "Accout No. : " << acno << endl;
+    cout << "Name : " << name << endl;
+    cout << "Account Type : " << type << endl;
+    cout << "Balance : " << bal << endl;
+  }
+  
+};
+int main()
+{
+  Bank b;
+  b.getDetails();  
+  b.deposit();
+  b.withdraw();
+  b.printDetails();
+}
