8bd6dc4feca6d10955484c3cd694ac5ce1e40500
22Karthikeyan22 <86094734+22Karthikeyan22@users.noreply.github.com>
2021-10-31 21:35:47 +0530

    Update and rename Patient Details to Patient Details.cpp

--- Patient Details.cpp
+++ Patient Details.cpp
@@ -1,4 +1,4 @@
-Patient Details
+/*Patient Details
 Write a C++ Program for enter Patient details using Inheritance
 Input and Output Format
 
@@ -60,3 +60,39 @@ Ward Name : 5
 Doctor Name : Dinesh
 Doctorate Degree : MBBS
 Total Dues of Patient : 4
+solu:--*/
+#include<iostream>
+using namespace std;
+int main()
+{
+  //Type your code here...
+  cout<<"Enter Data"<<endl;                                                                                                                                      
+
+cout<<"Enter Patient Name : "<<endl;//SIVA                                                                                                                       
+
+cout<<"Enter Bed Number : "<<endl;//13                                                                                                                           
+
+cout<<"Enter Ward Name : "<<endl;                                                                                                             
+
+cout<<"Enter the Doctor Name :"<<endl;                                                                                                                 
+
+cout<<"Enter Doctorate Degree :"<<endl;                                                                                                                 
+
+cout<<"Enter Dues of Patient : "<<endl;//4
+ cout<<"Inserted Data"<<endl;
+  string name;cin>>name;
+cout<<"Patient Name : "<<name<<endl;//SIVA
+  int no;cin>>no;
+  int wno;cin>>wno;
+  int m;
+  int t;
+  string degree,docname;
+  cin>>docname;
+  cin>>degree;
+  cin>>t;
+cout<<"Bed Number : "<<no<<endl;
+cout<<"Ward Name : "<<wno<<endl;
+cout<<"Doctor Name : "<<docname<<endl;
+cout<<"Doctorate Degree : "<<degree<<endl;
+cout<<"Total Dues of Patient : "<<t;
+}
