a0b650c7ca72a7dcadfa6a6d6c03dd7406dbe2a6
22Karthikeyan22 <86094734+22Karthikeyan22@users.noreply.github.com>
2021-10-31 21:15:52 +0530

    Update and rename Inheritance to Inheritance.cpp

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -1,4 +1,4 @@
-Color Cube
+/*Color Cube
 In leisure time, teacher gave a six-color cube to the students. To arrange with the similar color on each side. Initially, she doesn't give any timing to finish. Day by day she decreases the timing to solve six color cube. Students were very interesting to solve those puzzles with colorful cubes. Students keep on practicing the cubes and solve it within 3 minutes. so she decided to give them the tough cubes. She knows only the side. She wants to calculate the side of the square so that she can make her own cubes. Can you help her to calculate the cube sides by the c++ program?
 Input Format
 
@@ -27,3 +27,33 @@ Output (stdout)
 Enter the side :
 The square value is : 25
 The cube value is : 125
+solu:-*/
+#include<iostream>
+using namespace std;
+int main()
+{
+  //Type your code here...
+  string s;
+  cin>>s;
+  int a;
+  cin>>a;
+  char ch;
+  cin>>ch;
+  int m;
+  cin>>m;
+  char g;
+  cin>>g;
+  cout<<"Enter student's basic information:"<<endl;
+cout<<"Name:"<<endl;
+cout<<"Age:"<<endl;
+cout<<"Gender:"<<endl<<endl;
+ cout<<"Enter student's result information:"<<endl;
+cout<<"Total Marks: "<<endl;
+cout<<"Grade:"<<endl;
+cout<<"Name: "<<s<<endl;
+cout<<"Age: "<<a<<endl;
+cout<<"Gender: "<<ch<<endl;
+cout<<"Total Marks: "<<m<<endl;
+cout<<"Percentage: "<<(m*100)/500<<endl;
+cout<<"Grade: "<<g;
+}
