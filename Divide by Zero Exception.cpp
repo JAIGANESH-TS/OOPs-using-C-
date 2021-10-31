6dc8dd2ad48dfb9002b74a582520b6093301c9e8
22Karthikeyan22 <86094734+22Karthikeyan22@users.noreply.github.com>
2021-10-31 20:50:40 +0530

    Update and rename Divide by Zero Exception to Divide by Zero Exception.cpp

--- Divide by Zero Exception.cpp
+++ Divide by Zero Exception.cpp
@@ -1,4 +1,4 @@
-Divide by Zero Exception
+/*Divide by Zero Exception
 Rema has a task of dividing two decimal numbers. But the problem is, if the denominator has zero as input it has to throw the exception or it has to give the quotient value.Write the program for Handling the Divide by Zero Exception in C++ by using the runtime error class. Note: Use #include for run time exception.
 Input and Output Format
 
@@ -22,3 +22,20 @@ Input (stdin)
 
 Output (stdout)
 The quotient is 0.839546
+solu:--*/
+#include<iostream>
+#include<stdexcept>
+using namespace std;
+int main()
+{
+  float a,b;
+  cin >> a >> b;
+  try{
+    if (b==0)
+      throw runtime_error("Exception occurred\nMath error: Attempted to divide by Zero");
+    cout << "The quotient is " << a/b;
+  }
+  catch(const std::runtime_error& error){
+    cout << error.what();
+  }
+}
