29181b3e8a33524b68f0fc4a7a4a3b919c61ae59
22Karthikeyan22 <86094734+22Karthikeyan22@users.noreply.github.com>
2021-10-31 20:39:13 +0530

    Update and rename Home Work to Home Work.cpp

--- Home Work.cpp
+++ Home Work.cpp
@@ -1,4 +1,4 @@
-Home Work
+/*Home Work
 Ella is doing her Mathematics home work .Her Maths Teacher given a set of 10 questions to solve. She found the answers for many questions, but she can't able to find the solution for dividing the number by Zero. Help Ella to find answer for the question by Exception Handling.
 Input and Output Format:
 
@@ -24,3 +24,19 @@ Input (stdin)
 
 Output (stdout)
 Division by zero Exception
+solu:-*/
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a, b;
+  cin >> a >> b;
+  try{
+    if (b==0)
+      throw a;
+    cout << a/b;
+  }
+  catch(int x){
+    cout << "Division by zero Exception";
+  }
+}
