2488597dc226e3d2f579ee0b19f1e02886a812d3
22Karthikeyan22 <86094734+22Karthikeyan22@users.noreply.github.com>
2021-10-31 21:21:09 +0530

    Update and rename Blood Relations to Blood Relations.cpp

--- Blood Relations.cpp
+++ Blood Relations.cpp
@@ -1,4 +1,4 @@
-Blood Relations
+/*Blood Relations
 There were two friends playing in the park. They get bored by playing the same games. The two friends just absorbing the greenish surrounding. Joe said to ram see that tree, By my grandfather's guidance. That was planted on my 10th birthday. At that time, I named it as "Dora". The nearest tree was planted by my father's guidance on my 15th birthday. I named it as "Bujji".That was funny!!! Slowly, they started talking about nostalgic memories. Ram wants to know the names of her parent's names, grandparents' names. Can you help him to write the code in c++ program?
 Input and Output Format
 
@@ -18,3 +18,17 @@ Output (stdout)
 Enter the son name:
 Enter the father name:
 Enter the grand parent name:
+solu:--*/
+#include<iostream>
+using namespace std;
+int main()
+{
+  //Type your code here...
+  string a,b,c;
+  cin>>a;
+  cin>>b;
+  cin>>c;
+  cout<<"Enter the son name:"<<endl;
+cout<<"Enter the father name:"<<endl;
+cout<<"Enter the grand parent name:";
+}
