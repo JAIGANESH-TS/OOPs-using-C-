ca27799b1849bff25db380cc89025951842f9070
22Karthikeyan22 <86094734+22Karthikeyan22@users.noreply.github.com>
2021-10-31 20:48:38 +0530

    Update and rename ADA Company to ADA Company.cpp

--- ADA Company.cpp
+++ ADA Company.cpp
@@ -1,4 +1,4 @@
-ADA company
+/*ADA company
 William is the recruiter of a ADA company. He went for a recruitment process on a college. William conducted the first round. First round is conducted in a website and generated the score of the test. He set the cutoff mark as 50 and need to send the mail for individual candidates. When he enters the generated score to the platform it has to show that whether candidate cleared the first round or not. William is requesting you to write the program to find the exception of the test and to rethrow the candidate level for the second round.
 Input and Output Format
 
@@ -19,3 +19,22 @@ Input (stdin)
 
 Output (stdout)
 first round not cleared
+solu:--*/
+#include<iostream>
+using namespace std;
+int main()
+{
+  try{
+    int n;
+    cin >> n;
+    if (n<50)
+      throw "first round not cleared";
+   	else{
+      cout << "first round cleared" << endl;
+      cout << "eligible for second round";
+    }
+  }
+  catch(const char *s){
+    cout << s;
+  }
+}
