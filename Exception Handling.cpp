04991869433c0b19c076b36833bbd90ba8edfae2
22Karthikeyan22 <86094734+22Karthikeyan22@users.noreply.github.com>
2021-10-31 22:23:38 +0530

    Update and rename Exception Handling to Exception Handling.cpp

--- Exception Handling.cpp
+++ Exception Handling.cpp
@@ -1,4 +1,4 @@
-Exception Handling
+/*Exception Handling
 Ravi wants to do some arithmetic operations for his grocery store. He was planning to do addition, subtraction, multiplication, and division in a fast way so that he can reduce the time for calculation. Ravi wants to get two inputs as integer and to do the operation.
 
 If anyone of the input is zero it should print as "Bad Operation", if he enters any numbers or character instead of operators it should display that "It is not a Valid Operator" and if the entered values are the correct form it has to display the answer. Help Ravi to handle this situation by writing code. 
@@ -52,3 +52,39 @@ l
 
 Output (stdout)
 It is not a Valid Operator
+solu:--*/
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   // Try out your code here
+  int a;
+  cin>>a;
+  char b;
+  cin>>b;
+  if(a==0)
+    cout<<"Bad Operation";
+  else if(b=='+' || b=='-' || b=='*' || b=='/')
+  {
+    int c;
+    cin>>c;
+    if(c==0)
+      cout<<"Bad Operation";
+    else{
+    cout<<a<<" "<<b<<" "<<c<<" = ";
+    if(b=='+')
+      cout<<a+c;
+    if(b=='-')
+      cout<<a-c;
+    if(b=='*')
+      cout<<a*c;
+    if(b=='/')
+      cout<<a/c;
+  }}
+  else
+     cout<<"It is not a Valid Operator";
+    //cout << "Hello, World!";
+
+    return 0;
+}
