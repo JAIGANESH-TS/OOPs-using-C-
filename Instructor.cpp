901037bd993b851b458681a846c498f40ed0a316
22Karthikeyan22 <86094734+22Karthikeyan22@users.noreply.github.com>
2021-10-31 21:46:11 +0530

    Update and rename Instructor to Instructor.cpp

--- Instructor.cpp
+++ Instructor.cpp
@@ -1,4 +1,4 @@
-Instructor
+/*Instructor
 Write a C++ Program to print employee details using hybrid inheritance
 Input and Output Format
 
@@ -52,3 +52,57 @@ Name :  Ram
 Subject Name  :  Maths
 Teacher Name  :  Surya
 Department Name :  EEE
+solu:--*/
+#include<iostream>
+using namespace std;
+class Details
+{
+  public:
+  int id;
+  string name;
+  string sub;
+  string tname;
+  string dep;
+};
+int main()
+{
+   //Type your code here..
+  int no;
+  cout<<"Enter No. of Instructor Details You Want : "<<endl;
+  cin>>no;
+  Details arr[no];
+  int ind=0;
+  while(no-->0)
+  {
+    Details curr;
+    int a;
+    cout<<"Enter Id  :  "<<endl;                                                                                                                 
+cin>>a;
+    curr.id=a;
+cout<<"Enter Name :  "<<endl;                                                                                                                       
+string b;
+    cin>>b;
+    curr.name=b;
+cout<<"Enter Subject Name : "<<endl;                                                                                                                                                                
+string c;
+    cin>>c;
+    curr.sub=c;
+cout<<"Enter Teacher Name :  "<<endl;                                                                             
+string d;
+    cin>>d;
+    curr.tname=d;
+cout<<"Enter Department Name :  "<<endl;
+string e;
+    cin>>e;
+    curr.dep=e;
+    arr[ind++]=curr;
+  }
+  for(int i=0;i<ind;i++)
+  {
+   cout<<"Id :  "<<arr[i].id<<endl;
+cout<<"Name :  "<<arr[i].name<<endl;
+cout<<"Subject Name  :  "<<arr[i].sub<<endl;
+cout<<"Teacher Name  :  "<<arr[i].tname<<endl;
+cout<<"Department Name :  "<<arr[i].dep<<endl;
+  }
+}
